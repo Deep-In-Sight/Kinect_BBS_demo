be8c1465230941c505938226da092d9a15735688
Hoseung Choi <hopung@gmail.com>
2021-12-26 21:11:33 +0900

    socket communicator added

---
+++ bbsQt/comm/app_client.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+import os 
+#import sys
+import socket
+import selectors
+import traceback
+
+from . import libclient
+
+sel = selectors.DefaultSelector()
+
+
+def create_request(action, value):
+    if action == "search":
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action, value=value),
+        )
+    # File sender
+    elif action == "transfer":
+        return dict(
+            type="file",
+            encoding='binary',
+            content=value  # file name
+        )
+    else:
+        return dict(
+            type="binary/custom-client-binary-type",
+            encoding="binary",
+            content=bytes(action + value, encoding="utf-8"),
+        )
+
+
+def start_connection(host, port, request):
+    addr = (host, port)
+    print("starting connection to", addr)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    message = libclient.Message(sel, sock, addr, request)
+    sel.register(sock, events, data=message)
+
+
+def run_share_key(q_text, e_key, lock, debug=True):
+    host = '127.0.0.1'
+    port = 2345
+    action = "transfer"
+
+    e_key.wait()
+    if debug: print("[comm] HEAAN keys are ready")
+    fn_dict = q_text.get()
+    key_path = fn_dict['root_path']    
+
+    fn_tar = fn_dict['keys_to_share']
+    #print("[comm] found a encrypted data file:", fn_enc)
+    if os.path.isfile(fn_tar):
+        print("communicator found HEAAN keys:", fn_tar)
+        #e_enc.clear()
+
+    # File transfer request 
+    
+    if debug: print("[comm] sending keys", fn_tar)
+    
+    ans = share_key(host, port, action, fn_tar)
+
+    q_text.put(ans)
+
+
+def share_key(host, port, action, fn_key, debug=True):
+    # print("sending file to server.. ")
+    # time.sleep(3)
+
+    # ans = {'answer':"good"}
+    # print("got a response from server")
+    # queue.put(ans)
+
+    # while .... client에서 signal 받으면 끄기
+    # print("Sending quit signal.......................")
+    # e_quit.set()
+
+
+    #host, port = sys.argv[1], int(sys.argv[2])
+    #action, value = sys.argv[3], sys.argv[4]
+    #host = '10.100.82.55'
+    request = create_request(action, fn_key)
+    start_connection(host, port, request)
+    
+    try:
+        while True:
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        "main: error: exception for",
+                        f"{message.addr}:\n{traceback.format_exc()}",
+                    )
+                    message.close()
+            # Check for a socket being monitored to continue.
+            if not sel.get_map():
+                break
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+    
+    ans = {'answer':"good"}
+    print("[comm] got a response from server")
+    return ans
+    
+
+
+def query(queue, lock, e_enc, e_quit):
+    # print("sending file to server.. ")
+    # time.sleep(3)
+
+    # ans = {'answer':"good"}
+    # print("got a response from server")
+    # queue.put(ans)
+
+    # while .... client에서 signal 받으면 끄기
+    # print("Sending quit signal.......................")
+    # e_quit.set()
+
+
+    #host, port = sys.argv[1], int(sys.argv[2])
+    #action, value = sys.argv[3], sys.argv[4]
+    host = '10.100.82.55'
+    port = 2345
+    action = "transfer"
+
+
+    e_enc.wait()
+    print("[comm] Ciphertext ready")
+    fn_dict = queue.get()
+
+    fn_enc = fn_dict['fn_enc_skeleton']
+    #print("[comm] found a encrypted data file:", fn_enc)
+    if os.path.isfile(fn_enc):
+        print("communicator found ciphertext file:", fn_enc)
+        e_enc.clear()
+
+    # File transfer request 
+    request = create_request(action, fn_enc)
+    start_connection(host, port, request)
+
+    try:
+        while True:
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        "main: error: exception for",
+                        f"{message.addr}:\n{traceback.format_exc()}",
+                    )
+                    message.close()
+            # Check for a socket being monitored to continue.
+            if not sel.get_map():
+                break
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+
+    ans = {'answer':"good"}
+    print("got a response from server")
+    queue.put(ans)
--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from time import time
 import bbsQt
 
 from bbsQt.qtgui.qobj.QmainWindow import *
-from bbsQt.core import comm
+from bbsQt.comm import app_client
 from bbsQt.core.encryptor import HEAAN_Encryptor
 
 from PyQt5.QtWidgets import QApplication#, QMainWindow
 
 
-def run_qt_app(q1, lock, e_sk,):
+def run_qt_app(q1, q_text, lock, e_sk,):
     """
     QT 프로그램이 q1과 e_sk (이벤트 오브젝트)를 받아서 skeleton 데이터를 q1에 넣고, e_sk를 .set()해서 
     HEAAN_Encryptor에 Skeleton이 준비되었다고 신호를 주고싶은데, 어디로 q1과 e_sk를 넣어줘야할지 모르겠음. 
@@ -29,46 +29,40 @@ def run_qt_app(q1, lock, e_sk,):
     #quit
 
 
-from bbsQt.model.kinect_utils import skeleton_to_arr_direct
-
-def run_temp_qt(q1, lock, e_sk):
-    fn = "/home/hoseung/Work/Kinect_BBS_demo/G1/000/BT/bodytracking_data.pickle"
-    skeleton = pickle.load(open(fn, "rb"))
-    skeleton = skeleton_to_arr_direct(skeleton)
-    time.sleep(5)
-    q1.put({"skeleton":skeleton})
-    print("Loaded and put skeleton")
-    e_sk.set()
-
-from fase import HEAAN as he
-
-def encrypt(scheme, val, parms):
-    ctxt = he.Ciphertext()#logp, logq, n)
-    vv = np.zeros(parms.n) # Need to initialize to zero or will cause "unbound"
-    vv[:len(val)] = val
-    scheme.encrypt(ctxt, he.Double(vv), 
-					parms.n, parms.logp, parms.logq)
-    print("[ENCRYPT] 5")
-    del vv
-    return ctxt
+# def run_temp_qt(q1, lock, e_sk):
+#     fn = "/home/hoseung/Work/Kinect_BBS_demo/G1/000/BT/bodytracking_data.pickle"
+#     skeleton = pickle.load(open(fn, "rb"))
+#     skeleton = skeleton_to_arr_direct(skeleton)
+#     time.sleep(5)
+#     q1.put({"skeleton":skeleton})
+#     print("Loaded and put skeleton")
+#     e_sk.set()
 
 
-def run_encryptor(q1, lock, e_key, e_sk, e_enc, key_path="./"):
+def run_encryptor(q1, q_text, lock, e_key, e_sk, e_enc, key_path="./"):
     key_path = '/home/hoseung/Work/Kinect_BBS_demo/'
-    henc = HEAAN_Encryptor(e_key, lock, key_path)
+    henc = HEAAN_Encryptor(q_text, e_key, lock, key_path)
     #print(henc.prams.n)
     henc.start_encrypt_loop(q1, e_sk, e_enc)
 
 
 
+def run_communicator(e_key, q1, q_text, e_enc, e_quit, lock):
+    # 1. send keys to server and do quick check
+    e_key.wait()
+    app_client.run_share_key(q_text, e_key, lock)
+    e_enc.wait()
+    app_client.query(q1, lock, e_enc, e_quit)
+
 
     
 def main():
+    KEYPATH = "./"  
     lock = mplti.Lock()### 
     ctx = mplti.get_context('spawn') ###
 
     q1 = ctx.Queue(maxsize=8)
-    #q1 = ctx.Queue(maxsize=8)
+    q_text = ctx.Queue(maxsize=8)
 
     # Key existence
     e_key = multiprocessing.Event()
@@ -86,15 +80,15 @@ def main():
     e_quit = multiprocessing.Event()
     e_quit.clear()
 
-    p_socket = mplti.Process(target=comm.run, args=(q1, lock, e_enc, e_quit), daemon=False)
+    p_socket = mplti.Process(target=run_communicator, args=(e_key, q1, q_text, e_enc, e_quit, lock), daemon=False)
     p_socket.start()
 
     
-    p_enc = mplti.Process(target=run_encryptor, args=(q1, lock, e_key, e_sk, e_enc), daemon=False)
+    p_enc = mplti.Process(target=run_encryptor, args=(q1, q_text, lock, e_key, e_sk, e_enc), daemon=False)
     p_enc.start()
 
     #p_qt = mplti.Process(target=run_temp_qt, args=(q1, lock, e_sk), daemon=True) # 임시
-    p_qt = mplti.Process(target=run_qt_app, args=(q1, lock, e_sk), daemon=False) # 진짜
+    p_qt = mplti.Process(target=run_qt_app, args=(q1, q_text, lock, e_sk), daemon=False) # 진짜
     # ## signal quit()  
     p_qt.start()
 
